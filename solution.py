36c51a62824d5283f5b2807e54948c822c541720
Catherine243 <129989270+Catherine243@users.noreply.github.com>
2023-04-05 22:58:59 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,20 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 557932710 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    
+    import statistics as st
+    
+    y = [z - 270 for z in x]
+    
+    if(len(x) > 200):
+        x1 = sum(np.log(y))/len(x)
+    else:
+        x1 = st.median(np.log(y))
+    
+    return x1 # Ваш ответ
